8595a23dcfe669d68786d268844e4e2e6008e682
Angelgt3 <69768799+Angelgt3@users.noreply.github.com>
2023-04-30 15:16:36 -0600

    a

--- Backend/main.go
+++ Backend/main.go
@@ -66,7 +66,7 @@ func ejecutar(w http.ResponseWriter, r *http.Request) {
 }
 
 func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	//(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 	//(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
